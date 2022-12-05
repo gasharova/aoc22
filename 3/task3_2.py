42fc7b3b0b40e9ff1f91a294f96f2a85e84f74a3
unknown <andji.gash@gmail.com>
2022-12-05 23:33:42 +0100

    Removed prints from day 3 solution src

--- 3/task3_2.py
+++ 3/task3_2.py
@@ -17,28 +17,20 @@ def get_priority_for_items(items):
 
 line_counter = 1
 lines = []
-group_tracker_counter = 0
+
 
 with open('input3_2.txt') as inputfile:
     for line in inputfile:
         if line_counter == 4:
             group_item_type = get_shared_type(lines[0], lines[1], lines[2])
-            print(lines[0])
-            print(lines[1])
-            print(lines[2])
-            print(group_item_type)
-            if group_item_type:
-                group_tracker_counter += 1
             line_counter = 1
             lines.clear()
-            #print(shared_item)
             all_shared_items += group_item_type
-            
-        print(line)
+
         lines.append(line)
         line_counter += 1
         
 
 total_sum = get_priority_for_items(all_shared_items)
 print(total_sum)
-print(group_tracker_counter)
+
